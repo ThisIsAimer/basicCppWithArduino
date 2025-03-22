626d05637180e9f9c7af89f002df47a30b8d17f8
raunak <raunakbiswas05@gmail.com>
2025-03-22 18:01:01 +0530

    Pointer functions

--- basics/testingApps.cpp
+++ basics/testingApps.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using std::string;
 
+int addSums(int * a, int * b){
+
+    return (*a+*b);
+}
+
+
 int main(){
 
     // string ty = "hey there";
@@ -20,21 +26,28 @@ int main(){
 
     //----------------------------------------------------------------------------------------------------------
     //pointer experiment
-    int nums[5]= {3,4,5,6,7};// arrays are taken as pointers
+    // int nums[5]= {3,4,5,6,7};// arrays are taken as pointers
 
-    int *p = nums;
+    // int *p = nums;
 
-    printf("%d\n",nums);
-    printf("%d\n",nums+1);
-    printf("%d\n",nums);
-    printf("%d\n",*nums);
+    // printf("%d\n",nums);
+    // printf("%d\n",nums+1);
+    // printf("%d\n",nums);
+    // printf("%d\n",*nums);
 
 
-    for (int i = 0 ; i <5 ; i++){
-    // printf("%d, ",*nums);
-    // nums = nums +1; doesnt work
-        printf("%d, ",*p);
-        p++;
-    }
+    // for (int i = 0 ; i <5 ; i++){
+    // // printf("%d, ",*nums);
+    // // nums = nums +1; doesnt work
+    //     printf("%d, ",*p);
+    //     p++;
+    // }
+
+    int a = 100;
+    int b = 200;
+
+    //printf("\na + b = %d", addSums((int*)a,(int*)b)); //this doesnt work
+    printf("\na + b = %d", addSums(&a,&b));
+
 
 }
