27aae1d885090a2855ae31c342694c888515d662
raunak <raunakbiswas05@gmail.com>
2025-03-22 14:04:35 +0530

    some pointer examples

--- basics/testingApps.cpp
+++ basics/testingApps.cpp
@@ -3,20 +3,12 @@ using std::string;
 
 int main(){
 
-    string ty = "hey there";
+    // string ty = "hey there";
 
-    char hat[]= "hey hat";
-    int nums[5]= {3,4,5,6,7};
+    // char hat[]= "hey hat";
+    // int nums[5]= {3,4,5,6,7};
 
-    int *p = nums;
-
-
-    for (int i = 0 ; i <5 ; i++){
-    // printf("%d, ",*nums);
-    // nums = nums +1; doesnt work
-        printf("%d, ",*p);
-        p++;
-    }
+    // printf(hat);
 
     // int i = 0;
     // while (++i<10){
@@ -25,5 +17,24 @@ int main(){
     // }
 
     // printf("\n%d",i);
+
+    //----------------------------------------------------------------------------------------------------------
+    //pointer experiment
+    int nums[5]= {3,4,5,6,7};// arrays are taken as pointers
+
+    int *p = nums;
+
+    printf("%d\n",nums);
+    printf("%d\n",nums+1);
+    printf("%d\n",nums);
+    printf("%d\n",*nums);
+
+
+    for (int i = 0 ; i <5 ; i++){
+    // printf("%d, ",*nums);
+    // nums = nums +1; doesnt work
+        printf("%d, ",*p);
+        p++;
+    }
 
 }
