fbcd1db30149281be8acebcb1c877bb5e3b5c2c1
raunak <raunakbiswas05@gmail.com>
2025-03-23 10:47:44 +0530

    working with distructors and dinamic memory

--- basics/oopsClass.cpp
+++ basics/oopsClass.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 
 class vector{
     private:
@@ -7,25 +8,48 @@ class vector{
 
     int x,y;
 
+    char *name = nullptr;
+
 
     //constructors
-    vector(){ printf("\nA vector has been created");}
+    vector(){
+        
+        printf("\nA vector has been created");
+        name = new char [32] ;
+        strcpy(name,"hey how are you!");
+
+        }
+
+
     vector(int _x,int _y=2){
-         printf("\nA well defined vector has been created");
-         x = _x;
-         y = _y;
+        name = new char [32];
+        strcpy(name,"hey how are you!");
+        printf("\nA well defined vector has been created");
+        x = _x;
+        y = _y;
         }
  
 
     void print(){printf("\nthe positions of x,y is (%d,%d)",x,y);}
 
 
+        //distructor
+        ~vector(){ 
+            // marks the memory as available for reuse, but the pointer still holds the old address
+            delete [] name;
+            // destroys the previously allocated string
+            name = nullptr;
+
+            printf("\ndistructor\n");
+            printf("name: %s", name);
+        }
+
 };
 
 int main() {
 vector a;
 vector b(5,7);
-vector c(1);
+vector c = {1};
 
 a.x = 10;
 a.y = 20;
@@ -39,7 +63,9 @@ c.print();
 printf("\n");
 printf("\npointer p is pointing to");
 
-p->print(); // dereferencing operator for pointer objects
+// dereferencing operator for pointer objects
+p->x = 100;
+p->print();
 
     return 0;
 }
